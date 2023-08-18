18fd3853ca7d5e9f5f7c8665685778eab8684fd5
Anointeddrew <abah.d.andrew@gmail.com>
2023-08-18 11:26:09 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-	int larget;
+	int largest;
 
 	if (a > b && a > c)
 	{
@@ -24,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
