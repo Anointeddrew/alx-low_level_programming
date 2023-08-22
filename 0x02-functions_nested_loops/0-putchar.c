c77423e5caef5ba101343962c8a5dc40148582a0
Anointeddrew <abah.d.andrew@gmail.com>
2023-08-22 09:28:29 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * main - prints _putchar and a new line
  * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -13,9 +12,8 @@ int main(void)
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar('r');
 	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
