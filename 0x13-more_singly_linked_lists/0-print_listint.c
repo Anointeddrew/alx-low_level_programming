54173276d3b67c4ae628818d921c7189172da3a6
Anointeddrew <abah.d.andrew@gmail.com>
2023-09-26 21:59:14 -0700

    moresingly

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements in a list
+ * @h: linked lists of type listint_t to print
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n ", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *roam;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		roam = (*head)->next;
+		free(*head);
+		*head = roam;
+	}
+
+	*head = NULL;
+}
